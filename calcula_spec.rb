9d5e24350bec83704e396a454b8d0179d6a2c1ca
lmiguelespinoza <lmiguelespinoza@gmail.com>
2012-09-13 20:47:06 -0500

    Inicio PC1-test-1-5

--- calcula_spec.rb
+++ calcula_spec.rb
@@ -8,13 +8,16 @@ before do
    it 'suma vacio' do
      @calcula.suma('').must_equal 0
    end
-   it 'suma vacio 1' do
+   it 'suma 1' do
      @calcula.suma('1').must_equal 1
    end    
-   it 'suma vacio 1,2' do
+   it 'suma 1,2' do
      @calcula.suma('1,2').must_equal 3
    end 
-   it 'suma vacio 1,2,3' do
+   it 'suma 1,2,3' do
      @calcula.suma('1,2,3').must_equal 6
+   end 
+   it 'suma 1,2,3,4,5' do
+     @calcula.suma('1,2,3,4,5').must_equal 15
    end 
 end
