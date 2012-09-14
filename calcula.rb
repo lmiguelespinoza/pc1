64da20e41a111e7d9f013fb90356e967a9b2a385
lmiguelespinoza <lmiguelespinoza@gmail.com>
2012-09-13 20:42:37 -0500

    Inicio PC1-test-1-2

--- calcula.rb
+++ calcula.rb
@@ -1,7 +1,15 @@
 class Calcula
 def suma(cadena)
   resultado=0
-  resultado=1 if cadena=='1'  
+  posicion=0
+  longitud=cadena.length  
+  while longitud>0 do 
+        digito=cadena[posicion,1]        
+        resultado+=1 if digito=='1'  
+        resultado+=2 if digito=='2'  
+        longitud-=1
+        posicion+=1
+  end
   return resultado
 end
 end
--- calcula_spec.rb
+++ calcula_spec.rb
@@ -5,10 +5,13 @@ describe Calcula do
 before do
      @calcula=Calcula.new
    end
-   it 'suma vacio 1' do
-     @calcula.suma('1').must_equal 1
-   end 
    it 'suma vacio' do
      @calcula.suma('').must_equal 0
    end
+   it 'suma vacio 1' do
+     @calcula.suma('1').must_equal 1
+   end    
+   it 'suma vacio 1,2' do
+     @calcula.suma('1,2').must_equal 3
+   end 
 end
